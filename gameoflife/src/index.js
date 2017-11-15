517869b1b0947ae1fb562ec6b080ece40a03dbd9
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-11-15 00:34:31 -0500

    create Grid component

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Grid component 
+class Grid extends React.Component{
+    // all components should have a render method
+    render(){
+       // rendere method need to return an element
+       return (
+           <div>Grid</div>
+       ) 
+    }
+}
 
 // Main component
 class Main extends React.Component {
@@ -14,9 +24,12 @@ class Main extends React.Component {
     }
     // all components should have a render method
     render() {
-        // rendere method need to retern an element
+        // rendere method need to return an element
         return (<div>
             <h1>The Game of life</h1>
+            <Grid />
+            <h2>Generation: {this.state.generation}</h2>
+
         </div>
         );
     }
