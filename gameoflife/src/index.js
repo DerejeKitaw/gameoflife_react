177f0b1296cbac2c198904a358254c2708eb9b9d
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-11-15 00:26:16 -0500

    create main component

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -3,5 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-ReactDOM.render(<p>works</p>, document.getElementById('root'));
+// Main component
+class Main extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+            generation:0,
+        }
+    }
+    // all components should have a render method
+    render() {
+        // rendere method need to retern an element
+        return (<div>
+            <h1>The Game of life</h1>
+        </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
