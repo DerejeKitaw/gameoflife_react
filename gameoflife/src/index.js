f492df85e0e5aebfcbd718cf74ab6c19a1d0d304
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-11-15 02:04:34 -0500

    Box component created

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -2,39 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Box component
+class Box extends React.Component{
+    selectBox = () => {
+        this.props.selectBox (this.props.row, this.props.col);
+    }
+    // all components should have a render method
+    render(){
+        return(
+            <div
+            className={this.props.boxClass}
+            id={this.props.id}
+            onClick={this.selectBox} // this is not this.props.selectBox because we are going to create selectBox function in this component
+            />
+        );
+    }
+}
+
 // Grid component 
 class Grid extends React.Component{
     // all components should have a render method
     render(){
-        const width =this.props.cols *14;
-        var rowsArr =[];
+        const width = (this.props.cols * 14);
+		var rowsArr = [];
 
-        var boxClass = "";
-for (var i=0; i< this.props.rows; i++){
-    for (var j =0; j< this.props.cols; j++){
-        let boxId = i + "_" + j;
+		var boxClass = "";
+		for (var i = 0; i < this.props.rows; i++) {
+			for (var j = 0; j < this.props.cols; j++) {
+				let boxId = i + "_" + j;
 
-        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
-
-        rowsArr.push(
-            <Box 
-            boxClass = {boxClass}
-            key={boxId}
-            boxId={boxId}
-            row={i}
-            col={j}
-            selectBox={this.props.selectBox}
-            />
-        )
-    }
-}
+				boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+				rowsArr.push(
+					<Box
+						boxClass={boxClass}
+						key={boxId}
+						boxId={boxId}
+						row={i}
+						col={j}
+						//selectBox={this.props.selectBox}
+					/>
+				);
+			}
+		}
 
        // rendere method need to return an element
+       // when we are inside a tag we use two curly brace and outside we use single brace
        return (
             <div className="grid" style={{width: width}}>
-            {{rowsArr}}
+            {rowsArr} 
            </div>
-       ) 
+       );
     }
 }
 
@@ -44,27 +61,30 @@ class Main extends React.Component {
     constructor(){
         super();
         // speed , rows and columns will be refernced in the state. So created outside state
-        this.speed=100;
-        this.rows=30;
-        this.cols=50; // columns
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50; // columns
 
         this.state={
             generation:0,
             // create a grid rows x by cols and turn off initialy
-            gridFull: Array(this.ros).fill().map(() => Array(this.cols).fill(false))
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
         }
     }
+
+    
     // all components should have a render method
     render() {
         // rendere method need to return an element
-        return (<div>
+        return (
+            <div>
             <h1>The Game of life</h1>
             
             <Grid 
                 gridFull={this.state.gridFull}
                 rows={this.rows}
                 cols={this.cols}
-                selectBox={this.selectBox}
+                //selectBox={this.selectBox}
             />
             <h2>Generation: {this.state.generation}</h2>
 
